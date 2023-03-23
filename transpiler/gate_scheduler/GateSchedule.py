d3d92b08c534a449f77ac7ee2e29ba28653e1dfa
ANGEL ALVAREZ <angel7wonderzxw@gmail.com>
2023-03-23 16:38:51 -0400

    feat: GateSchedule general class implemented

--- transpiler/gate_scheduler/GateSchedule.py
+++ transpiler/gate_scheduler/GateSchedule.py
@@ -1,3 +1,6 @@
+from transpiler.aqipt_pulses import *
+from transpiler.rydberg_circuits import *
+
 def coupling_detuning_constructors(couplings: List, detunings : List, omega_coup : 20, omega_detu = 0) -> tuple():
     coupling1 = {}
     for i, coupling in enumerate(couplings):
@@ -11,3 +14,24 @@ def coupling_detuning_constructors(couplings: List, detunings : List, omega_coup
         detuning1['Detuning'+str(i)] = [levels, omega_detu, detuning]
         
     return coupling1, detuning1
+
+class GateSchedule():
+    
+    def __init__(self,
+                 t_start: float,
+                 freq: float,
+                 pair: list
+                ) -> None:
+        
+        self.t_start = t_start
+        self.t_end = t_start
+        self.freq = freq
+        self.pair = pair
+        self.n_qubits = len(pair)
+        self.omega = 2*np.pi*freq
+        self.q_schedule = None
+        
+    def __call__(self):
+        return self.q_schedule   
+        
+        
