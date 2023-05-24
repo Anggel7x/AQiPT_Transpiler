5d5a2e6b453cb5e1130145a07a96eb1352e20120
ANGEL ALVAREZ <angel7wonderzxw@gmail.com>
2023-05-24 02:15:53 -0400

    feat: CPhase refactoring

--- transpiler/gate_scheduler/CphaseSchedule.py
+++ transpiler/gate_scheduler/CphaseSchedule.py
@@ -1,6 +1,7 @@
 from transpiler.aqipt_pulses import *
 from transpiler.rydberg_circuits import *
 from .GateSchedule import *
+from transpiler.gate_scheduler.UxySchedule import UxySchedule
 from transpiler.gate_scheduler.RxSchedule import RxSchedule
 
 class CphaseSchedule(GateSchedule):
@@ -22,20 +23,20 @@ class CphaseSchedule(GateSchedule):
     def _schedule(self):
         
         t_start = self.t_start
+        t_2 = self.t_2
         freq = self.freq
         shape = self.shape
-        t_2 = self.t_2
+        A = 0.206441*(2*np.pi*freq)
         c_pair, t_pair = self.pair[0], self.pair[1]
-           
+            
         # 1 -> r
         rt1 = RxSchedule(theta=np.pi, t_start=t_start, freq=freq, shape=shape, pair=t_pair)
         rc1 = RxSchedule(theta=np.pi, t_start=t_start, freq=freq, shape=shape, pair=c_pair)
 
         # r -> 1 + t_2
-        rt2 = RxSchedule(theta=np.pi, t_start=rt1.t_end + t_2, freq=freq, shape=shape, pair=t_pair)
-        rc2 = RxSchedule(theta=np.pi, t_start=rc1.t_end + t_2, freq=freq, shape=shape, pair=c_pair)
+        rt2 = UxySchedule(theta = A, t_start=rt1.t_end + t_2, freq=freq, shape=shape, pair=t_pair)
+        rc2 = UxySchedule(theta = A, t_start=rc1.t_end + t_2, freq=freq, shape=shape, pair=c_pair)
 
         rt1.q_schedule.add_function(rt2.q_schedule.coupling_pulses["Coupling0"][2], "Coupling0")
         rc1.q_schedule.add_function(rc2.q_schedule.coupling_pulses["Coupling0"][2], "Coupling0")
-        
         self.q_schedule = (rc1, rt1) # control and target schedules
