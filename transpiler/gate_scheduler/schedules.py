93d6ad8d07e335b7fa024079896eeca52642afd4
ANGEL ALVAREZ <angel7wonderzxw@gmail.com>
2023-05-24 02:10:44 -0400

    fix: reubication of schedules importation

---
+++ transpiler/gate_scheduler/schedules.py
@@ -0,0 +1,13 @@
+# 1 qubit schedules
+
+from .UxySchedule import UxySchedule
+from .RxSchedule import RxSchedule
+from .RySchedule import RySchedule
+from .RzSchedule import RzSchedule
+
+# 2 qubits schedules
+
+from .CUxySchedule import CUxySchedule
+from .PCUxySchedule import PCUxySchedule
+from .CphaseSchedule import CphaseSchedule
+from .XYSchedule import XYSchedule
